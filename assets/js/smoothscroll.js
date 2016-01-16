2c2fa4819c19db945d858fa77474fcd247684267
sohilpandya <sohilpandya@me.com>
2016-01-16 21:14:07 +0000

    smooth scrolling now stopping in correct place for both big and small screen fixed hamburger menu so that it now autocloses when on mobile

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
+	        'scrollTop': $target.offset().top -60
+	    }, 1000, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
