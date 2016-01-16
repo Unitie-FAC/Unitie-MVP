f89dfe6dd754315ee8ac927ced22e0f4597663a2
tormod17 <tormodsmith@gmail.com>
2016-01-16 11:29:46 +0000

    succes message

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,11 @@ function successMessage(inputId, outputId){
 	var message ="Thank you for registering your details, we'll be in contact shortly";
     var submitBtn = document.getElementById('submit').addEventlistenr;
     var output = document.getElementById(outputId);
+        output.innerHTML="";
     var newEl   = output.innerHTML += "<h2>"+message+"</h2>";
+    $('form').addClass('hide');
     });
+
 
 
 }
