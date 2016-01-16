9e2d55f0ebf07aa37188acb57ed55af96b608ecd
tormod17 <tormodsmith@gmail.com>
2016-01-16 22:30:58 +0000

    using show and hide css #85

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,58 @@
-  function hideNShow(){
-        		$('#successMessage').addClass('hide');
-        	    $('form')[0].reset();
-        	    $('form').addClass('show');
-  }
+  function hideNShow(hideEl, showEl) {
+
+      var inputs = $("form").find(':input');
+      var ArrInputs = Object.keys(inputs).slice(0, -4);
+      var requiredArr = [];
+
+      ArrInputs.forEach(function(ele) {
+          if (inputs[ele].className === 'form-control required') {
+              requiredArr.push(inputs[ele]);
+          }
+      });
+
+      var requiredEmpty = false;
+      requiredArr.forEach(function(ele) {
+          if (ele.value === '') {
+              requiredEmpty = true;
+              console.log('error');
+          }
+      });
+      if (requiredEmpty === false) {
+          $(hideEl).removeClass('show');
+          $(hideEl).addClass('hide');
+          $(showEl).removeClass('hide');
+          $(showEl).addClass('show');
+
+      }
+ }
 
 
 
-function successMessage(inputId, outputId) {
+      /*function successMessage(inputId, outputId) {
 
-    document.getElementById(inputId).addEventListener("click", function(e) {
+          document.getElementById(inputId).addEventListener("click", function(e) {
 
-        var submitBtn = document.getElementById('submit').addEventlistenr;
-        var output = document.getElementById(outputId);
-        output.innerHTML = "";
-        
-          
-   	    var inputs = $("form").find(':input');
-     	var ArrInputs = Object.keys(inputs).slice(0, -4);
-      	var requiredArr = [];
-       	 
-       	 ArrInputs.forEach(function(ele){
-       	 	if (inputs[ele].className === 'form-control required'){
-       	 		requiredArr.push(inputs[ele]); 
-       	 	}
-       	 });
+              var submitBtn = document.getElementById('submit').addEventlistenr;
+              var output = document.getElementById(outputId);
+              output.innerHTML = "";
+              
+                
+         	  
+             	$('form').addClass('hide');
+             	$('#successMessage').addClass('hide');
+              var message = "Thank you for registering your details, we'll be in contact shortly";
+              var newEl = output.innerHTML += "
+           
+              }
 
-       var  requiredEmpty = false;
-       requiredArr.forEach(function(ele) {
-            if (ele.value === '') {
-        	   requiredEmpty = true;
-        	   console.log('error');
-       		}
-        }); 
-       	if (requiredEmpty === false){
-        var message = "Thank you for registering your details, we'll be in contact shortly";
-        var newEl = output.innerHTML += "<div id='successMessage' class='text-center'><h2>" + message + "</h2><button onclick='hideNShow()'>Register</butotn></a>";
-        $('form').addClass('hide');
-        }
-
-      
+            
 
 
-    });
+          });
 
 
 
-}
+      }
 
-successMessage('submit', 'messageOut');
+      //successMessage('submit', 'messageOut');
+      */
